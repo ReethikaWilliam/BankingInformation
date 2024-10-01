ed2a7dd9bed5a998f8d811b0ae9c43cbf68821ae
ReethikaWilliam <reethikawilliam@gmail.com>
2024-10-01 21:42:54 +0530

    Dev

--- src/org/sample/HomeWork.java
+++ src/org/sample/HomeWork.java
@@ -1,8 +1,14 @@
 package org.sample;
 
 public class HomeWork {
+	
+public static void empId() {
+		System.out.println("employee name is reethika");
+	}
 public static void main(String[] args) {
 	System.out.println("jj");
+	
+	
 }
 	
 }
